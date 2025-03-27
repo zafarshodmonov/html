8945e76379a0a51f9dbb8726a272671787ad27f5
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2025-03-27 15:03:28 +0500

    z

---
+++ hello.py
@@ -0,0 +1,15 @@
+M = {
+    1: 'Hello',
+    2: 'World',
+    3: 'Python',
+    4: 'Programming',
+    5: 'Language',
+    6: 'Pythonista',
+    7: 'Programmer',
+    8: 'Developer',
+    9: 'Software',
+    10: 'Engineer',
+    11: 'Computer',
+    12: 'Science',
+    13: 'Perspective Transformation'
+}
